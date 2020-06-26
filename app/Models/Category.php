480e4f14d6d424c4a1c2af6f9c99d2d50856be10
Wagner Mengue <dev.mengue@gmail.com>
2020-06-25 23:54:55 -0300

    added uuid to category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,16 +2,27 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Uuid;
+
+    public $incrementing = false;
 
     protected $fillable = [
-        'name', 'description', 'is_active'
+        'name',
+        'description',
+        'is_active',
     ];
 
-    protected $date = ['deleted_at'];
+    protected $date = [
+        'deleted_at',
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+    ];
 }
---
+++ app/Traits/Uuid.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Traits;
+
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+
+trait Uuid
+{
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($object) {
+            $object->id = RamseyUuid::uuid4();
+        });
+    }
+}
