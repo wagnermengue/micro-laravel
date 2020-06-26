c675c405a31af10c29a9ca47b31a93801d8e03dd
Wagner Mengue <dev.mengue@gmail.com>
2020-06-25 21:18:32 -0300

    added update and delete categories on api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean',
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -26,11 +31,7 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'name' => 'required|max:255',
-            'is_active' => 'boolean',
-        ]);
-
+        $this->validate($request, $this->rules);
         return Category::create($request->all());
     }
 
@@ -54,7 +55,9 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $this->validate($request, $this->rules);
+        $category->update($request->all());
+        return $category;
     }
 
     /**
@@ -65,6 +68,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }
