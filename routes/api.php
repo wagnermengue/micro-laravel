7f99b797dd9c11e610e9f485f95d83f0858f6862
Wagner Mengue <dev.mengue@gmail.com>
2020-07-11 12:09:26 -0300

    fix bind controller name videos

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::group(['namespace' => 'Api'], function () {
     Route::resource('categories', 'CategoryController', $exceptEditAndCreate);
     Route::resource('genres', 'GenreController', $exceptEditAndCreate);
     Route::resource('cast_members', 'CastMemberController', $exceptEditAndCreate);
-    Route::resource('videos', 'VideosController', $exceptEditAndCreate);
+    Route::resource('videos', 'VideoController', $exceptEditAndCreate);
 });
