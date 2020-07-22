4a29e76478dd23819dfa92fc1a8fcb527b04cf86
Wagner Mengue <dev.mengue@gmail.com>
2020-07-21 23:36:13 -0300

    fix import TestCase

--- tests/Unit/Models/Traits/UploadFileUnitTest.php
+++ tests/Unit/Models/Traits/UploadFileUnitTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit\Models\Traits;
 
 use Illuminate\Http\UploadedFile;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 use Tests\Stubs\Models\Traits\UploadFileStub;
 
 class UploadFileUnitTest extends TestCase
