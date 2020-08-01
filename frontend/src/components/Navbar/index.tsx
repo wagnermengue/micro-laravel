b7a53dd76c45f8cfa5bf4d7111312add83963693
Wagner Mengue <dev.mengue@gmail.com>
2020-08-01 08:40:19 -0300

    added menu

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import {AppBar, Button, Theme, Toolbar, Typography} from "@material-ui/core";
+import {AppBar, Button, IconButton, Menu, MenuItem, Theme, Toolbar, Typography} from "@material-ui/core";
 import logo from "../../static/img/logo.png";
 import {makeStyles} from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
@@ -24,6 +25,23 @@ export const Navbar: React.FC = () => {
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="open drawer"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                >
+                    <MenuIcon/>
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    open={false}
+                >
+                    <MenuItem>
+                        Categorias
+                    </MenuItem>
+                </Menu>
                 <Typography className={classes.title}>
                     <img src={logo} alt="Codeflix" className={classes.logo}/>
                 </Typography>
