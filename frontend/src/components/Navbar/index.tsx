6de4c98c68a1bc57467a3cc078dfca3ecc17d5f2
Wagner Mengue <dev.mengue@gmail.com>
2020-08-01 08:30:10 -0300

    dynamic size logo

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
-import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
+import {AppBar, Button, Theme, Toolbar, Typography} from "@material-ui/core";
 import logo from "../../static/img/logo.png";
 import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         backgroundColor: "#000000"
     },
     title: {
         flexGrow: 1,
         textAlign: "center"
+    },
+    logo: {
+        width: 100,
+        [theme.breakpoints.up('sm')] : {
+            width: 170
+        }
     }
-});
+}));
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
@@ -19,7 +25,7 @@ export const Navbar: React.FC = () => {
         <AppBar>
             <Toolbar className={classes.toolbar}>
                 <Typography className={classes.title}>
-                    <img src={logo} alt="Codeflix"/>
+                    <img src={logo} alt="Codeflix" className={classes.logo}/>
                 </Typography>
                 <Button color="inherit">
                     Login
