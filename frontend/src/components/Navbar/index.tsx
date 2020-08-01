7adac5a4bdb729e34564a80a8696d1df5dd40d21
Wagner Mengue <dev.mengue@gmail.com>
2020-08-01 09:03:15 -0300

    added menu hooks and fix path run docker cloudbuild

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -22,6 +22,13 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export const Navbar: React.FC = () => {
     const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleOpen = (event:any) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
     return (
         <AppBar>
             <Toolbar className={classes.toolbar}>
@@ -31,14 +38,20 @@ export const Navbar: React.FC = () => {
                     aria-label="open drawer"
                     aria-controls="menu-appbar"
                     aria-haspopup="true"
+                    onClick={handleOpen}
                 >
                     <MenuIcon/>
                 </IconButton>
                 <Menu
                     id="menu-appbar"
-                    open={false}
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handleClose}
+                    anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+                    transformOrigin={{vertical: "top", horizontal: "center"}}
+                    getContentAnchorEl={null}
                 >
-                    <MenuItem>
+                    <MenuItem onClick={handleClose}>
                         Categorias
                     </MenuItem>
                 </Menu>
