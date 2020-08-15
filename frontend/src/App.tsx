d783d9938f26b7261abc9f7db23845edd5d97a42
Wagner Mengue <dev.mengue@gmail.com>
2020-08-15 13:09:19 -0300

    fix react envs and added theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar";
-import {Box} from "@material-ui/core";
+import {Box, MuiThemeProvider} from "@material-ui/core";
 import AppRouter from "./routes/AppRouter";
 import {BrowserRouter} from "react-router-dom";
 import Breadcrumbs from "./components/Breadcrumbs";
+import theme from "./theme";
 
 function App() {
     return (
         <React.Fragment>
-            <BrowserRouter>
-                <Navbar/>
-                <Box paddingTop={"70px"}>
-                    <Breadcrumbs/>
-                    <AppRouter/>
-                </Box>
-            </BrowserRouter>
+            <MuiThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <Navbar/>
+                    <Box paddingTop={"70px"}>
+                        <Breadcrumbs/>
+                        <AppRouter/>
+                    </Box>
+                </BrowserRouter>
+            </MuiThemeProvider>
         </React.Fragment>
     );
 }
