678832246ec5db26bcf50cf8a12c5c0bb65033df
Wagner Mengue <dev.mengue@gmail.com>
2021-09-27 21:57:35 -0300

    added getters to count upload files in progress

--- frontend/src/components/SnackbarUpload/index.tsx
+++ frontend/src/components/SnackbarUpload/index.tsx
@@ -7,6 +7,9 @@ import {makeStyles} from "@material-ui/core/styles";
 import {useState} from "react";
 import classnames from "classnames";
 import UploadItem from "./UploadItem";
+import {useSelector} from "react-redux";
+import {Upload, UploadModule} from "../../store/upload/types";
+import {countInProgress} from "../../store/upload/getters";
 
 const useStyles = makeStyles((theme: Theme) => ({
     card: {
@@ -53,11 +56,17 @@ const SnackbarUpload = React.forwardRef<any, SnackbarUploadProps>((props, ref) =
     const {closeSnackbar} = useSnackbar();
     const [expanded, setExpanded] = useState(true);
 
+    const uploads = useSelector<UploadModule, Upload[]>(
+        (state) => state.upload.uploads
+    );
+
+    const totalInProgress = countInProgress(uploads);
+
     return (
         <Card ref={ref} className={classes.card}>
             <CardActions classes={{ root: classes.cardActionRoot}}>
                 <Typography variant="subtitle2" className={classes.title}>
-                    Fazendo upload de 10 vídeo(s)
+                    Fazendo upload de {totalInProgress} vídeo(s)
                 </Typography>
                 <div className={classes.icons}>
                     <IconButton
